38449ebeb0848bfa2caafc87a0ce73cd81938fc1
muradmy00 <murad.my00@gmail.com>
2024-07-18 01:47:26 +0600

    project

--- JS/script.js
+++ JS/script.js
@@ -19,10 +19,51 @@ function handle(product,isIncrease){
     if(product == "laptop"){
         productTotal = newProductCount * 245000;
     }
-    else{
-         productTotal = newProductCount * 176999;
+    if(product == "phone"){
+        productTotal = newProductCount * 176999;
     }
+
 
     document.getElementById(product+'-total').innerText = productTotal;
 
+    handleCheckout();
+
+}
+
+
+
+
+function handleCheckout(){
+
+    const laptopCount = getItems("laptop");
+
+    const phoneCount = getItems("phone");
+
+    const totalPrice = parseInt(laptopCount) * 245000 + parseInt(phoneCount) * 176999;
+
+    document.getElementById("sub_total").innerText = totalPrice;
+
+    const tax = Math.round(totalPrice * 0.1);
+
+    document.getElementById("tax_amount").innerText = tax;
+
+    const grandTotal = (totalPrice * tax);
+
+     document.getElementById("grand_total").innerText = grandTotal;
+
+}
+
+
+function getItems(product){
+    const productInput = document.getElementById(product);
+
+    const productCount = parseInt(productInput.value);
+
+    return productCount;
+}
+
+
+function Checkout(){
+    alert("Thanks for purchasing with us!")
+    location.reload();
 }
